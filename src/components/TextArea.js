eb25c80e4214f40e6e6339f0128551e5078a5504
Morris Buel <63753944+SBS-MBUEL@users.noreply.github.com>
2021-05-28 06:26:32 -0700

    fixed textarea

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -5,33 +5,21 @@ class TextArea extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            value : props.value != null ? props.value : "Not Set"
+            value : props.value != "" ? props.value : "Not Set"
         }
         
         this.trackChanges = this.trackChanges.bind(this);
-        // this.setCursorPos = this.setCursorPos.bind(this);
     }
 
-    // setCursorPos() {
-    //     const textarea = document.querySelector(`#${this.props.label}-inputForNotes`);
-    //     console.log(`#${this.props.label}-inputForNotes`);
-    //     console.log(textarea);
-    //     const cursor_pos = this.state.value.length;
-    
-    //     textarea.setSelectionRange(cursor_pos, cursor_pos);
-    //     textarea.focus();
-    // }
-
     trackChanges(e) {
-        e.preventDefault()
 
-        e.stopPropagation();
         console.log(e.target);
+
         this.setState({
-            value: e.target.textContent
+            value: e.target.value
         })
 
-        this.props.trackChanges(this.props.label, e.target.textContent);
+        this.props.trackChanges(this.props.label, e.target.value);
     }
     
     render() {
@@ -47,9 +35,6 @@ class TextArea extends React.Component {
                     onChange={this.trackChanges} 
                     id={`${label}-inputForNotes`}
                     value={this.state.value}
-                    // ref={this.setCursorPos} 
-                    selectionStart={startingPoint}
-                    selectionEnd={startingPoint + this.state.value.length}
                 >
                     
                 </textarea>
