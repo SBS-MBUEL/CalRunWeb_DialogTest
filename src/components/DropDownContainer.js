27c3a8d0330427751b7c4da485c3e050c25392d9
Morris Buel <63753944+SBS-MBUEL@users.noreply.github.com>
2021-06-14 09:39:13 -0700

    drop state is working correctly now

--- src/components/DropDownContainer.js
+++ src/components/DropDownContainer.js
@@ -10,6 +10,9 @@ class DropDownContainer extends React.Component {
             userValue : props.userValue,
             dropState: false
         }
+
+        this.wrapperRef = React.createRef();
+
         this.changeDropItem = this.changeDropItem.bind(this);
         this.toggleDropdown = this.toggleDropdown.bind(this);
         this.handleBodyClick = this.handleBodyClick.bind(this);
@@ -25,9 +28,13 @@ class DropDownContainer extends React.Component {
         this._isMounted = false;
       }
 
-    handleBodyClick(e)
-    {
-        if (e.className !== "drop-item" && this._isMounted) {
+      /**
+       * when an element outside of ref (in return below) is clicked, it will toggle the state of the drop down
+       * @param {DOM} e 
+       */
+    handleBodyClick(e) {
+
+        if (this.wrapperRef && this.wrapperRef.current && !this.wrapperRef.current.contains(e.target)) {
             this.toggleState(true);
         }
 
@@ -81,7 +88,7 @@ class DropDownContainer extends React.Component {
         const { row, index } = this.props;
         return (
             <div key={`${index}-dropdown-container`} className={`dropdown ${this.state.dropState ? 'is-active' : ''}`}>
-                <div className="dropdown-trigger options">
+                <div ref={this.wrapperRef} className="dropdown-trigger options">
                     <span onClick={this.toggleDropdown} className="controlLink" aria-haspopup="true" aria-controls="dropdown-menu">
                         <span className={`dropdown-toggle`} data-toggle="dropdown">{this.state.userValue}</span>
                         <span className="icon is-small">
