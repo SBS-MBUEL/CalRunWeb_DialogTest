161a70a5e081776aa82d9a19f1d9368a81a61cb8
Morris Buel <63753944+SBS-MBUEL@users.noreply.github.com>
2021-07-07 11:34:23 -0700

    fixed buttons for subs

--- src/utils/ArrayUtils.js
+++ src/utils/ArrayUtils.js
@@ -15,12 +15,12 @@ function RemoveItemFromArray(array, index) {
             if(idx != index) //index you want to remove
             return item;
         });
+        return results;
 
     } catch (ex) {
         // TODO: not sure what error could happen from above, need to research - try to catch with testing
+        return -1;
     }
-
-    return results;
 }
 /**
  * Searched the document for the passed in element, and the returns a filtered array of those objects with the regex text content
@@ -42,6 +42,33 @@ function FilterElementsOnTextContent(query, regex) {
         // TODO: need to try and define error and capture / test for
     }
     return results;
+}
+
+/**
+ * The goal of this method is to find a unique index in the settings object by supplying two key value pairs.
+ * @param {object} settings object
+ * @param {string} key1 field name, for example label
+ * @param {string} value1 of field1
+ * @param {string} key2 field name, for example value
+ * @param {string} value2 of field2
+ * @return {number}
+ */
+function FindUniqueIndexInSettings(settings, key1, value1, key2, value2) {
+    if ((!settings && typeof(settings) !== 'object') || (!key1 && typeof(key1) !== 'string') || (!key2 && typeof(key2) !== 'string') || (!value1 && typeof(value1) !== 'string') || (!value2 && typeof(value2) !== 'string') ) {
+        // problem with arguments
+        return -1;
+    }
+    try {
+        let indice = settings.map((el, index) => el.ItemName === key && el.ConfigurationArea === tabName.toLowerCase() ? index : undefined).filter((a, b) => a !== undefined);
+
+        if (indice.length > 1) {
+            // search query is not returning a unique result.
+            return -1; 
+        }
+        return indice[0];
+    } catch (er) {
+        return -1;
+    }
 }
 
 
